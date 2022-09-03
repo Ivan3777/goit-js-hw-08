d37737e840773d2e05464afb8f4f2dbed42e74a0
Ivan Vodopianov <vis3010@gmail.com>
2022-09-03 21:42:54 +0300

    end task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import storageAPI from './storage';
+var throttle = require('lodash.throttle');
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', handleInput);
+
+form.addEventListener('submit', handleSubmit);
+
+function handleInput (e) {
+    const {name, value} = e.target;
+
+    let savedData = storageAPI.load('formKey');
+    savedData = savedData ? savedData : {};
+    savedData[name] = value;
+    storageAPI.save('formKey', savedData);
+}
+
+function initPage () {
+savedData = storageAPI.load('formKey');
+if (!savedData) {
+    return;
+}
+Object.entries(savedData).forEach(([name, value]) => {
+    form.elements[name].value = value;
+})
+}
+
+function handleSubmit (e) {
+    e.preventDefault();
+    const {
+        elements: {email, message}
+    } = e.currentTarget;
+
+    console.log({email: email.value, message: message.value});
+
+    e.currentTarget.reset();
+    storageAPI.remove('formKey');
+}
+
+
 

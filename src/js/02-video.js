975b80901bbab0655192964381241467726a29cd
Ivan Vodopianov <vis3010@gmail.com>
2022-09-03 19:32:41 +0300

    end task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+
+const time = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(time || 0.1)
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
